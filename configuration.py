ae5814150fa41b6cef30123a3b1f766f9ef70d5e
11XtaZ11 <mehanizator3@yandex.ru>
2024-02-21 16:47:15 +0000

    Автотест и базы данных

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://a4cf81d8-c595-4b1b-9bbb-d525e313338d.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFO = "/api/v1/orders/track?t="
